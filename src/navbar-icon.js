329ff1cfd5435a9eb39c771da08fa376a7cf3255
solomiyasabat <solomiyasabat@gmail.com>
2023-01-19 12:59:54 +0200

    fixed photo-categories list position

---
+++ src/navbar-icon.js
@@ -0,0 +1,9 @@
+//document.querySelector(".nav-ellipse").style.borderBottomColor = "transparent";
+//document.querySelector(".nav-ellipse").style.borderLeftColor = "transparent";
+//border-bottom-color: transparent;
+//border-left-color: transparent;
+//rotate: (-45deg);
+
+//const element = document.getElementsByClassName(".nav-ellipse");
+//let text = element.getAttribute("type");
+//console.log(text);
