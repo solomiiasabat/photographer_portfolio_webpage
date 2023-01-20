4c5f23072829f3a157f0ad7878dba88c69e6f1df
solomiyasabat <solomiyasabat@gmail.com>
2023-01-20 12:19:06 +0200

    navbar-icon design changes after clicking

--- src/navbar-icon.js
+++
@@ -1,6 +0,0 @@
-const element = document.getElementsByClassName(".nav-ellipse");
-let open = element.getAttribute("aria-expanded");
-
-if (open === true) {
-  document.querySelector(".nav-ellipse").className = "semi-nav-ellipse";
-}
